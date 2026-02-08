57e074ca1adc528d9a503f454b46c61557622534
HashCoder <hashcoderslab@gmail.com>
2026-02-08 14:55:51 +0530

    "feat: Db configured"

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# server
+# Configuring EFCORE:
+
+1.  Install required Packages (3)
+2.  appsetting.json
+    "ConnectionStrings": {
+    "DefaultString": "Server=.;Database=Project_Management;TrustedConnection=True;TrustServerCertificate=True" }
+
+3.  Data Folder
+4.  DbContext File/Class
+5.  AppDbContext class registered in Program.cs
--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,17 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+//db registering
+;
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString")));
 
 var app = builder.Build();
 
