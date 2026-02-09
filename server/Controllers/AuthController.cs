79d37662a5adcdca3a53b253e0492f8179995f08
HashCoder <hashcoderslab@gmail.com>
2026-02-09 22:55:29 +0530

    bug: Auth Modified

--- server/Controllers/AuthController.cs
+++ server/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using server.Data;
@@ -17,7 +18,7 @@ namespace server.Controllers
 
         public AuthController(AppDbContext dbContext, JwtHelper jwtHelper)
         {
-           _dbContext = dbContext;
+            _dbContext = dbContext;
             _jwtHelper = jwtHelper;
         }
 
@@ -29,15 +30,27 @@ namespace server.Controllers
                 return BadRequest("User does not exist");
             }
 
-            
-            if (!VerifyPassword(loginDTO.Password,user.HashedPassword)) {
+
+            if (!VerifyPassword(loginDTO.Password, user.HashedPassword)) {
                 return BadRequest("Username or Password is Invalid");
             }
 
-            List<string> roles = ["Admin", "User", "Manager"];
+            var roles = await _dbContext.UserRoles
+                .Where(ur => ur.UserId == user.UserId)
+                .Select(ur => ur.Role!.RoleName)
+                .ToListAsync();
             string token = _jwtHelper.GenerateToken(user, roles);
 
-            return Ok(token);
+            var response = new AuthResponseDTO
+            {
+                Token=token,
+                Username = user.UserName,
+                Email=user.UserEmail,
+                ExpiresIn = DateTime.UtcNow.AddMinutes(120),
+                Roles = roles
+            };
+
+            return Ok(response);
         }
 
 
@@ -49,7 +62,7 @@ namespace server.Controllers
             {
                 return BadRequest("User Already Exists");
             }
-            
+
 
             var user = new User
             {
@@ -59,16 +72,45 @@ namespace server.Controllers
             };
 
             await _dbContext.Users.AddAsync(user);
+
+            var defaultRole = await _dbContext.Roles.FirstOrDefaultAsync(role => role.RoleName == "Developer");
+
+            if (defaultRole != null) {
+                var newUserRole = new UserRole
+                {
+                    User = user,
+                    Role = defaultRole
+                };
+                await _dbContext.UserRoles.AddAsync(newUserRole);
+            }
+
+
             await _dbContext.SaveChangesAsync();
 
             return Ok(user);
         }
 
+
+
+
         [Authorize]
-        [HttpGet("test")]
-        public string Test()
+        [HttpGet("me")]
+        public async Task<ActionResult> GetCurrentUser()
         {
-            return "It is working";
+            //token -> userid
+            var userid = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+
+            
+
+            if(userid == null)
+            {
+                return Unauthorized();
+            }
+
+            int id = int.Parse(userid.Value);
+
+            var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.UserId == id);
+            return  Ok(user);
         }
         private string HashPassword(string password)
         {
