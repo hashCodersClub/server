3259a21afe6eff0d21fd1b94cf0c3dbab28576ab
HashCoder <hashcoderslab@gmail.com>
2026-02-09 15:58:47 +0530

    Feat: Authentication & Authorization Added

---
+++ server/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.DTOs.AuthDTO;
+using server.Helpers;
+using server.Models;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly JwtHelper _jwtHelper;
+
+        public AuthController(AppDbContext dbContext, JwtHelper jwtHelper)
+        {
+           _dbContext = dbContext;
+            _jwtHelper = jwtHelper;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginDTO loginDTO) {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.UserName == loginDTO.UserName);
+
+            if (user == null) {
+                return BadRequest("User does not exist");
+            }
+
+            
+            if (!VerifyPassword(loginDTO.Password,user.HashedPassword)) {
+                return BadRequest("Username or Password is Invalid");
+            }
+
+            List<string> roles = ["Admin", "User", "Manager"];
+            string token = _jwtHelper.GenerateToken(user, roles);
+
+            return Ok(token);
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDTO userDTO)
+        {
+            //unique email
+            if (await _dbContext.Users.AnyAsync(u => u.UserEmail == userDTO.Email))
+            {
+                return BadRequest("User Already Exists");
+            }
+            
+
+            var user = new User
+            {
+                UserEmail = userDTO.Email,
+                UserName = userDTO.UserName,
+                HashedPassword = HashPassword(userDTO.Password),
+            };
+
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [Authorize]
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "It is working";
+        }
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        private bool VerifyPassword(string text,string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(text, hash);
+        }
+    }
+}
---
+++ server/Helpers/JwtHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using server.Models;
+
+namespace server.Helpers
+{
+    public class JwtHelper
+    {
+
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtHelper(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(User user,List<string> roles)
+        {
+            
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+
+            var credentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.UserId.ToString()),
+                new Claim(ClaimTypes.Name,user.UserName)
+            };
+
+            foreach (string role in roles) {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    signingCredentials: credentials,
+                    expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+                   issuer:_jwtSettings.Issuer,
+                   audience:_jwtSettings.Audience
+                    );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
