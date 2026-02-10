8c8f4d70941e72dc22231f0262747661dbc23407
HashCoder <hashcoderslab@gmail.com>
2026-02-10 15:22:55 +0530

    feat: Project CRUD

---
+++ server/DTOs/ProjectDTO/ProjectResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace server.DTOs.ProjectDTO
+{
+    public class ProjectResponseDTO
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+
+        public string CreatorName { get; set; } = String.Empty;
+    }
+}
